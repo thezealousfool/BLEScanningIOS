00c798b2f0f5c5b1a7b666eb8831dd9e1cfddbf2
Vivek Roy <thezealousfool@gmail.com>
2019-11-02 09:15:50 -0400

    Initial Commit

---
+++ BLEScanning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLEScanning
+//
+//  Created by imac on 11/2/19.
+//  Copyright © 2019 Vivek Roy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
